aacfbdb6f3f90740edffd3947329b92cccec59a9
Hassani Assinaoui <hassani.assinaoui@gmail.com>
2022-11-19 14:29:17 +0100

    correction product.js

--- front/js/product.js
+++ front/js/product.js
@@ -4,8 +4,6 @@ import { getCart, setCart } from "./functions/localstorage.js";
 const productId =  new URL(window.location.href).searchParams.get("id");
 
 
-
-
 //récupération de l'article
 displayProduct(productId);
 
@@ -44,128 +42,33 @@ addToCartBtn.addEventListener("click", addToCart);
 function addToCart() {
 
     const colorChoice = document.querySelector("#colors").value;
-    const quantityChoice = document.querySelector("#quantity").value;
+    const quantityChoice = parseInt(document.querySelector("#quantity").value);
 
     if (quantityChoice > 0 && quantityChoice <=100 && quantityChoice != 0 && colorChoice != 0) { 
         let cart = getCart()
         //on verifie si le produit existe dans le panier
         if( productId in cart){
             //verifier si la couleur existe dans le panier
-            //elle existe on incremente la valeur de la quantité
-
-
-            //si elle n'existe pas, on crée cette couleur avec sa quantité
-            cart[productId][colorChoice]= quantityChoice
+            if(colorChoice in  cart[productId]){
+                 //elle existe on incremente la valeur de la quantité
+                 cart[productId][colorChoice] +=  quantityChoice
+            }else{
+                //si elle n'existe pas, on crée cette couleur avec sa quantité
+                cart[productId][colorChoice] = quantityChoice
+            }
         }//le canapé n'existe pas dans le panier
         else{
-        
-            const color = document.querySelector("#colors").value;
-            const quantitySelected = document.querySelector("#quantity").value;
-            
+
             cart[productId] = {
-                [color]: quantitySelected
+                [colorChoice]: quantityChoice
             }
-        
-         
         }
         setCart(cart)
         
         alert("Le produit est dans votre panier !");  
-       /*  if (localStorage.getItem("cart")) {
-    
-            let productCart = JSON.parse(localStorage.getItem("cart"));
-            console.log(productCart);
-
-            let colorKanap = document.querySelector("#colors").value;
-            let qtyKanap = document.querySelector("#quantity").value;
-
-            const resultFind = productCart.find(
-                (el) => el.idKanap === productId && el.colorKanap === colorKanap);
-
-                //Si le produit est déjà dans le panier
-                console.log("result find est egal a :");
-                console.log(resultFind);
-                console.log("fin result find");
-
-                if (resultFind) {
-                    console.log("resultfind kanap = " + resultFind.qtyKanap);
-                    console.log("qtykanap = " + qtyKanap);
-                    let newQuantite = parseInt(qtyKanap) + parseInt(resultFind.qtyKanap);
-                    console.log("newQtt est egal a : " + newQuantite);
-                    resultFind.qtyKanap = newQuantite;
-                    localStorage.setItem("cart", JSON.stringify(productCart));
-                    console.log("productCart egal :");
-                    console.log(productCart);
-                    console.log("fin productCart");
-
-                //Si le produit n'est pas dans le panier
-                } else {
-                    
-                    let productCart = JSON.parse(localStorage.getItem("cart"));
-
-                    let idKanap = productId;
-                    let nameKanap = document.querySelector("#title").textContent;
-                    let colorKanap = document.querySelector("#colors").value;
-                    let qtyKanap = document.querySelector("#quantity").value;
-                    let imgKanap = img.src; 
-                    let altImg = img.alt;
-                    let priceKanap = document.querySelector("#price").textContent;
-                    
-                    console.log(img);
-                    console.log(idKanap, nameKanap, colorKanap, qtyKanap, imgKanap, altImg, priceKanap);
-                
-                    let productCartObj = {
-                        idKanap : productId,
-                        nameKanap : nameKanap,
-                        colorKanap : colorKanap,
-                        qtyKanap  : qtyKanap,
-                        imgKanap : imgKanap,
-                        altImg : altImg,
-                        priceKanap : priceKanap
-                    };
-                
-                    productCart.push(productCartObj);
-                
-                    let objCart = JSON.stringify(productCart);
-                    localStorage.setItem("cart", objCart);
-                
-                    alert("Dans votre panier !");
-                }
-
-        } else {
-
-            let productCart = [];
-
-            let idKanap = productId;
-            let nameKanap = document.querySelector("#title").textContent;
-            let colorKanap = document.querySelector("#colors").value;
-            let qtyKanap = document.querySelector("#quantity").value;
-            let imgKanap = img.src; 
-            let altImg = img.alt;
-            let priceKanap = document.querySelector("#price").textContent;
-            
-            console.log(img);
-            console.log(idKanap, nameKanap, colorKanap, qtyKanap, imgKanap, altImg, priceKanap);
-        
-            let productCartObj = {
-                idKanap : productId,
-                nameKanap : nameKanap,
-                colorKanap : colorKanap,
-                qtyKanap  : qtyKanap,
-                imgKanap : imgKanap,
-                altImg : altImg,
-                priceKanap : priceKanap,
-            };
-        
-            productCart.push(productCartObj);
-        
-            let objCart = JSON.stringify(productCart);
-            localStorage.setItem("cart", objCart);
-        
-            alert("Dans votre panier !");    
-        } */
+       
     }else{
-        alert ("veuillez renseigner tous les champs")
+        alert ("Veuillez renseigner tous les champs !")
     }
 }
 
@@ -173,7 +76,7 @@ function addToCart() {
  * 
 cart = {
     1432432 : {
-        "red": 3
+        "red": 3 + 2 
         "blue": 2
     },
     2343242:{
