8c19fcefc1ec8b5cdb288ba6fad9e0c974142d18
Hassani Assinaoui <hassani.assinaoui@gmail.com>
2023-01-19 18:02:13 +0100

    projet final complet

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,7 +1,35 @@
-const id = new URL(window.location.href).searchParams.get("confirmation");
-console.log(id);
+let messageDisplayed = false;
 
-const orderId = document.getElementById("orderId");
-orderId.innerHTML = id;
+function getConfirmationId() {
+  if (messageDisplayed) {
+    return;
+  }
+  messageDisplayed = true;
+  const url = new URL(window.location.href);
+  return url.searchParams.get("confirmation");
+}
 
-localStorage.clear();
+function getConfirmationId() {
+    const url = new URL(window.location.href);
+    return url.searchParams.get("confirmation");
+  }
+  
+  const id = getConfirmationId();
+  console.log(id);
+  
+  const orderId = document.getElementById("orderId");
+  if (orderId) {
+    orderId.textContent = id;
+  } else {
+    console.error("Erreur : élément orderId introuvable sur la page.");
+  }
+  //Message de remerciement a la clientele
+  const message = document.createElement("p");
+  message.textContent = "Merci pour votre achat!";
+  
+  const confirmation = document.querySelector(".confirmation");
+  confirmation.appendChild(message);
+
+  //sessionStorage.clear() si on souhaite uniquement effacer les données de la session en cours sinon l'usage de localStorage.clear()
+  sessionStorage.clear();
+  
